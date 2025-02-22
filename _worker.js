7560994d89e1fd684f530651f7608ecf6e7bdb82
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-02-23 05:38:16 +0700

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,49 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    const rules = {
+      '/avatar': {
+        targetHost: 'secure.gravatar.com',
+        pathTransform: (path, prefix) => '/avatar' + path.replace(prefix, ''),
+        service: 'Gravatar'
+      },
+      '/comment': {
+        targetHost: 'i0.wp.com',
+        pathTransform: (path, prefix) => '/comment.bibica.net/static/images' + path.replace(prefix, ''),
+        service: 'Artalk & Jetpack'
+      },
+      '/': {
+        targetHost: 'i0.wp.com',
+        pathTransform: (path) => '/bibica.net/wp-content/uploads' + path,
+        service: 'Jetpack'
+      }
+    };
+
+    const rule = Object.entries(rules).find(([prefix]) => url.pathname.startsWith(prefix));
+
+    if (!rule) {
+      return new Response(`Path not supported: ${url.pathname}`, { status: 404 });
+    }
+
+    const targetUrl = new URL(request.url);
+    const [prefix, config] = rule;
+    targetUrl.hostname = config.targetHost;
+    targetUrl.pathname = config.pathTransform(url.pathname, prefix);
+    targetUrl.search = url.search;
+
+    const response = await fetch(targetUrl, {
+      headers: { 'Accept': request.headers.get('Accept') || '*/*' }
+    });
+
+    return new Response(response.body, {
+      headers: {
+        'content-type': 'image/webp',
+        'last-modified': response.headers.get('last-modified'),        
+        'link': response.headers.get('link'),
+        'X-Cache': response.headers.get('x-nc'),
+        'X-Served-By': `Cloudflare Pages & ${config.service}`
+      }
+    });
+  }
+};
